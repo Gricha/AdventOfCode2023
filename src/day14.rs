9316aadaf7a78f557f26d8ddfbab7ef47d5890e5
Greg Pstrucha <875316+Gricha@users.noreply.github.com>
2023-12-15 13:32:57 -0800

    Days 14 & 15

--- src/day14.rs
+++ src/day14.rs
@@ -1,3 +1,176 @@
-pub fn part1() {}
+use std::collections::HashMap;
 
-pub fn part2() {}
+use crate::utils::read_input;
+
+// fn print_map(map: &Vec<Vec<char>>) {
+//     for row in map {
+//         for col in row {
+//             match col {
+//                 '.' => print!("."),
+//                 '#' => print!("#"),
+//                 'O' => print!("O"),
+//                 _ => unreachable!(),
+//             }
+//         }
+//         println!();
+//     }
+// }
+
+enum Direction {
+    N,
+    E,
+    S,
+    W,
+}
+
+fn roll(map: &mut Vec<Vec<char>>, dir: Direction) {
+    match dir {
+        Direction::N => {
+            for j in 0..map[0].len() {
+                for i in 0..map.len() {
+                    let mut x = i;
+                    let y = j;
+                    if i > 0 && map[x][y] == 'O' {
+                        while x > 0 {
+                            if map[x - 1][y] == '.' {
+                                map[x][y] = '.';
+                                map[x - 1][y] = 'O';
+                                x -= 1;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Direction::S => {
+            for j in (0..map[0].len()).rev() {
+                for i in (0..map.len()).rev() {
+                    let mut x = i;
+                    let y = j;
+                    if i < map.len() - 1 && map[x][y] == 'O' {
+                        while x < map.len() - 1 {
+                            if map[x + 1][y] == '.' {
+                                map[x][y] = '.';
+                                map[x + 1][y] = 'O';
+                                x += 1;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Direction::W => {
+            for j in 0..map[0].len() {
+                for i in 0..map.len() {
+                    let x = i;
+                    let mut y = j;
+                    if j > 0 && map[x][y] == 'O' {
+                        while y > 0 {
+                            if map[x][y - 1] == '.' {
+                                map[x][y] = '.';
+                                map[x][y - 1] = 'O';
+                                y -= 1;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Direction::E => {
+            for j in (0..map[0].len()).rev() {
+                for i in (0..map.len()).rev() {
+                    let x = i;
+                    let mut y = j;
+                    if j < map[0].len() - 1 && map[x][y] == 'O' {
+                        while y < map[0].len() - 1 {
+                            if map[x][y + 1] == '.' {
+                                map[x][y] = '.';
+                                map[x][y + 1] = 'O';
+                                y += 1;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn count_load(map: &Vec<Vec<char>>) -> i64 {
+    let max_rows = map.len();
+    let mut count = 0;
+    for (idx, row) in map.iter().enumerate() {
+        for col in row {
+            if *col == 'O' {
+                count += (max_rows - idx) as i64;
+            }
+        }
+    }
+    count
+}
+
+pub fn part1() {
+    let mut map = read_input("inputs/day14.txt")
+        .into_iter()
+        .map(|x| x.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+    roll(&mut map, Direction::N);
+    println!("{}", count_load(&map));
+}
+
+fn roll_cycle(map: &mut Vec<Vec<char>>) {
+    roll(map, Direction::N);
+    roll(map, Direction::W);
+    roll(map, Direction::S);
+    roll(map, Direction::E);
+}
+
+fn deep_clone(map: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut clone_map = vec![];
+    for row in map {
+        clone_map.push(row.clone());
+    }
+    clone_map
+}
+
+pub fn part2() {
+    let mut map = read_input("inputs/day14.txt")
+        .into_iter()
+        .map(|x| x.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+    let mut cycle_map: HashMap<Vec<Vec<char>>, Vec<i64>> = HashMap::new();
+    let mut cycle_lookup: Vec<Vec<Vec<char>>> = vec![deep_clone(&map)];
+    let mut idx = 0;
+
+    let mut cycle_length = 0;
+    let mut first_idx = 0;
+    loop {
+        idx += 1;
+        roll_cycle(&mut map);
+        cycle_lookup.push(deep_clone(&map));
+
+        if cycle_map.contains_key(&map) {
+            cycle_map.get_mut(&map).unwrap().push(idx);
+
+            if cycle_map.get(&map).unwrap().len() == 2 {
+                cycle_length = cycle_map.get(&map).unwrap()[1] - cycle_map.get(&map).unwrap()[0];
+                first_idx = cycle_map.get(&map).unwrap()[0];
+                break;
+            }
+        } else {
+            cycle_map.insert(deep_clone(&map), vec![idx]);
+        }
+    }
+
+    let cycle_rest = (1000000000 - first_idx) % cycle_length;
+    let proper_cycle = &cycle_lookup[(cycle_rest + first_idx) as usize];
+    println!("Day 14 Part 2: {}", count_load(proper_cycle));
+}
--- src/day15.rs
+++ src/day15.rs
@@ -1,3 +1,118 @@
-pub fn part1() {}
+use std::str::FromStr;
 
-pub fn part2() {}
+use itertools::Itertools;
+
+use crate::utils::read_input;
+
+fn hash(str: &str) -> u64 {
+    let mut val: u64 = 0;
+    for c in str.chars() {
+        let ascii = c as u8;
+        val += ascii as u64;
+        val *= 17;
+        val %= 256;
+    }
+    val
+}
+
+pub fn part1() {
+    let sum = read_input("inputs/day15.txt")
+        .into_iter()
+        .flat_map(|x| x.split(',').map(|x| x.to_string()).collect_vec())
+        .map(|x| hash(&x))
+        .sum::<u64>();
+
+    println!("Day 15, Part 1: {}", sum);
+}
+
+enum Operation {
+    Remove,
+    Insert(u64),
+}
+
+struct Command {
+    label: String,
+    box_idx: usize,
+    operation: Operation,
+}
+
+impl FromStr for Command {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.ends_with('-') {
+            let label = s[..s.len() - 1].to_string();
+            let box_idx = hash(&label) as usize;
+            Ok(Command {
+                label,
+                operation: Operation::Remove,
+                box_idx,
+            })
+        } else {
+            let (label, val) = s.split_once('=').unwrap();
+            let h = hash(label);
+
+            Ok(Command {
+                label: label.to_owned(),
+                operation: Operation::Insert(val.parse().unwrap()),
+                box_idx: h as usize,
+            })
+        }
+    }
+}
+
+fn print_data(data: &Vec<Vec<(String, u64)>>) {
+    for (i, d) in data.iter().enumerate() {
+        if d.is_empty() {
+            continue;
+        }
+        println!("Box {}: {:?}", i + 1, d);
+    }
+}
+
+pub fn part2() {
+    let mut data: Vec<Vec<(String, u64)>> = vec![];
+    for i in 0..256 {
+        data.push(vec![]);
+    }
+
+    let commands = read_input("inputs/day15.txt")
+        .into_iter()
+        .flat_map(|x| x.split(',').map(|x| x.to_string()).collect_vec())
+        .map(|x| x.parse::<Command>().unwrap())
+        .collect_vec();
+
+    for c in commands {
+        match c.operation {
+            Operation::Insert(val) => {
+                if data[c.box_idx].iter().any(|x| x.0 == c.label) {
+                    data.get_mut(c.box_idx)
+                        .unwrap()
+                        .iter_mut()
+                        .filter(|x| x.0 == c.label)
+                        .for_each(|x| x.1 = val);
+                } else {
+                    data.get_mut(c.box_idx).unwrap().push((c.label, val));
+                }
+            }
+            Operation::Remove => {
+                data.get_mut(c.box_idx).unwrap().retain(|x| x.0 != c.label);
+            }
+        }
+    }
+
+    let total_value = data
+        .into_iter()
+        .enumerate()
+        .map(|(idx, boxx)| {
+            let value = boxx
+                .iter()
+                .enumerate()
+                .map(|(box_pos, focal_value)| (box_pos + 1) as u64 * focal_value.1)
+                .sum::<u64>();
+            value * (idx + 1) as u64
+        })
+        .sum::<u64>();
+
+    println!("Day 15, Part 2: {}", total_value);
+}
